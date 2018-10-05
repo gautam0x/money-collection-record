d1f71e9946ca83d81ae0c0a9aa472f5eff76774e
gautamrwx <gautam.404@protonmail.com>
2018-10-06 01:44:33 +0530

    Added Page to edit client information

---
+++ edit_client_info.php
@@ -0,0 +1,153 @@
+<?php 
+    //include all config files
+    require 'config_files/db_config.php';
+    require 'config_files/defined_function.php';
+    require 'config_files/session_tracking.php';
+    require 'config_files/timezone_config.php';
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Edit Client</title>
+<link rel="stylesheet" type="text/css" href="css/page_layout.css" />
+<link rel="stylesheet" type="text/css" href="css/forums_and_tables.css" />
+<link rel="stylesheet" type="text/css" href="css/navbar_design.css" />
+</head>
+<body>
+<div class="main-container-outer-wrapper">
+<div class="main-container-inner-wrapper">
+
+
+<!--***********************************
+>>>>>>>> Header Section <<<<<<<<<<<
+************************************-->
+<?php require 'header.php'; ?>
+
+
+<!--**********************************
+>>>>>>>>> Navigation Bar <<<<<<<<<<<
+***********************************-->
+<div class="navbar-wrapper">
+<div class="navbar">
+<ul>
+    <li><a href="index.php">					Home</a></li>
+    <li><a href="add_client.php">				Add Client</a></li>
+    <li><a href="update_balance.php">			Update Balance </a></li>
+    <li><a href="delete_client.php">			Delete Client</a></li>
+</ul>
+</div>
+</div>
+
+<?php
+// GET id value from redirected page and dispaly its data in input box
+$alert_msg = "";
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id']))
+{
+    $id     = htmlspecialchars($_GET['id']);
+    $sql    = "SELECT name,information FROM peoples WHERE id=$id";
+    $result = $conn->query($sql);
+    
+    if($result->num_rows > 0)
+    {
+        $row = $result->fetch_assoc() ;
+        $old_client_name     = $row['name']; // take backup to rename indivisual table
+        $new_client_name     = $row['name']; 
+        $information         = $row['information'];
+    }
+}
+?>
+
+<?php
+// Action after forum submit
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_condition']))
+{
+	$id                 = htmlspecialchars($_POST['id']);
+	$information        = htmlspecialchars($_POST['information']);
+	$new_client_name    = htmlspecialchars($_POST['new_name']);
+	$old_client_name    = htmlspecialchars($_POST['old_name']);
+
+	$old_table_name = find_indivisual_table_name($old_client_name , $id);
+	$new_table_name	= find_indivisual_table_name($new_client_name , $id);
+
+	// Update Master Table Record
+	$sql = "UPDATE peoples SET name = '$new_client_name' , information = '$information' WHERE id = $id ";
+	if($conn->query($sql))
+	{
+		// if new- name is same as older-name we dont need to change indivisual table name
+		if($old_table_name == $new_table_name)
+		{
+			$alert_msg = "<div class='sucess-msg-alert'>Data Inserted Sucessfully</div>";
+		}
+		// else change indivisual table name
+		else
+		{
+			$sql = "RENAME TABLE $old_table_name TO $new_table_name"; 
+			if($conn->query($sql))
+				$alert_msg = "<div class='sucess-msg-alert'>Data Inserted Sucessfully</div>";
+			else
+				$alert_msg = "<div class='failure-msg-alert'>Data Corrupted :( </div>";
+		}
+	}
+	else
+	{
+		$alert_msg = "<div class='failure-msg-alert'>Data cannot be insertrd</div>";
+	}
+}
+?>
+
+<!--**********************************
+>>>>>>>>> Content Area <<<<<<<<<<<
+***********************************-->
+<div class="content-area-wrapper" >
+<div class="content-area" >
+<div class="content-holder-min-width">
+    
+    <div class="container-box-wrapper">
+        <div class="container-box-header">
+            Update Client Information
+        </div>
+        
+        <div class="container-box-body">
+		<center>
+			<div id="alertBox"><?php echo $alert_msg; ?></div>
+            
+			<form onsubmit="return confirm('Do you Want To Re-Update')"  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+			<input name="form_condition" type="hidden" value="1" />
+			<input name="id" type="hidden" value="<?php echo $id ?>" />
+			<input name="old_name" type="hidden" value="<?php echo $new_client_name ?>" />
+			<table class="forum-holder-table">
+				<tr>
+					<td width="50%"> Name</td>
+					<td width="50%"><input class="medium-width" name="new_name" type="text" value="<?php echo $new_client_name; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="50%">Information</td>
+					<td width="50%"><input class="medium-width" maxlength="35" name="information" type="text" value="<?php echo $information; ?>" /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Submit" / > </td>
+				</tr>
+			</table>
+			</form>
+		</center>
+        </div>
+    </div>
+    
+    
+</div> 
+</div>
+</div>
+  
+
+<!--**********************************
+>>>>>>>>>   Footer Area  <<<<<<<<<<<
+************************************-->
+<?php require 'footer.php' ; ?>
+
+</div>
+</div>
+</body>
+</html>
