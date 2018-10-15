e51576c785bdc21fad04cea1d7c506819f7a3322
gautamrwx <gautam.404@protonmail.com>
2018-10-15 06:22:57 +0530

    Added Message to display after delete action perform

--- delete_client.php
+++ delete_client.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     //include all config files
     require 'config_files/db_config.php';
     require 'config_files/defined_function.php';
@@ -44,6 +44,20 @@
 
 
 
+<?php
+// Respone message genrate after data delete
+$alert_msg = "";
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['response']))
+{
+  if($_GET['response'] == '1')
+    $alert_msg = "<div class='sucess-msg-alert'>Client deleted sucessfully</div>";
+  else if($_GET['response'] == '0')
+    $alert_msg = "<div class='failure-msg-alert'>Sorry : Failed to delete client</div>";
+  else
+    $alert_msg = "";
+}
+?>
+
 <!--**********************************
 >>>>>>>>> Content Area <<<<<<<<<<<
 ***********************************-->
@@ -53,7 +67,7 @@
 
     <div class="container-box-wrapper">
         <div class="container-box-body">
-
+          <div id="alertBox"><?php echo $alert_msg; ?></div>
             <table class="index-table" width="100%">
 				<tr>
 					<th>Name</th>
@@ -75,12 +89,12 @@
 					while($row = $result->fetch_assoc())
 					{
 						// variable to Highlight Only loan account
-						if($row['account_type'] == 'loan') 
-							$account_type = "<span style=color:red; > ( LOAN )</span>" ;  
-						else  
+						if($row['account_type'] == 'loan')
+							$account_type = "<span style=color:red; > ( LOAN )</span>" ;
+						else
 							$account_type = "" ;
 						?>
-						
+
 						<tr class="<?php echo $tr_class_alter ; ?>">
 						<td><?php echo $row['name'].$account_type ;?></td>
 						<td><?php echo $row['information'] ; ?></td>
@@ -88,7 +102,7 @@
 						<td><?php echo $row['total_balance'] ; ?></td>
 						<td><a class="link-as-button" href=delete_client_confirm.php?id=<?php echo $row['id'];?>&name=<?php echo urlencode($row['name']);?> >Delete</a></td>
 						</tr>
-						
+
 						<?php
                         //Condition to change row color by altering TableRow class name in each loop
                         if($tr_class_alter == "type_1")   $tr_class_alter = "type_2";
